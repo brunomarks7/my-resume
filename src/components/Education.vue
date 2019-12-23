76efd7a1a31776812aaf6b61750d23db3378df1b
Bruno Marques <brunomarks@gmail.com>
2019-12-23 00:54:34 -0300

    ajustes

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,29 +1,28 @@
 <template>
   <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
     <div class="w-100">
-      <h2 class="mb-5">Education</h2>
+      <h2 class="mb-5">Educação</h2>
 
+      <p>Estudo praticamente todos os dias. Faz parte da minha rotina fazer cursos através da Udemy, RocketSeat, PluralSight, Cod3r e outros canais de Youtube</p>
+      <p class="mb-5">Tenho interesse de estudo em áreas adjacentes, o que enriquece a forma de lidar com meu ecommerce. Frequentemente estudo SEO, UX, Growth Hacking além de me manter por dentro de ferramentas de automação, ERPs, Gateways logísticos e de pagamentos</p>
+      <br>
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <h3 class="mb-0">University of Bazqux</h3>
-          <div class="subheading mb-3">Postgraduate education in computer science</div>
-          <div>Bachelor's degree</div>
-          <p>GPA: 3.53</p>
+          <h3 class="mb-0">Fatec BS (Santos)</h3>
+          <div class="subheading mb-3">Graduação como Tecnólogo em Processamento de Dados</div>
         </div>
         <div class="resume-date text-md-right">
-          <span class="text-primary">August 2007 - May 2011</span>
+          <span class="text-primary">2009 - 2010 (4 ciclo)</span>
         </div>
       </div>
 
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
         <div class="resume-content">
-          <h3 class="mb-0">Foobar High School</h3>
-          <div class="subheading mb-3">Electical engineering</div>
-          <div>Computer science tack</div>
-          <p>GPA: 4.16</p>
+          <h3 class="mb-0">ETEC - Centro Paula Souza</h3>
+          <div class="subheading mb-3">Técnico em Processamento de Dados</div>
         </div>
         <div class="resume-date text-md-right">
-          <span class="text-primary">August 2003 - May 2007</span>
+          <span class="text-primary">2004 - 2006</span>
         </div>
       </div>
     </div>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -7,10 +7,52 @@
         <div class="resume-content">
           <h3 class="mb-0">Front-End Developer</h3>
           <div class="subheading mb-3">Apê11</div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            Sou responsável pelo Front-End do site, assim como decisões sobre tecnologias, viabilidade técnica com relação a design, além de integrações entre APIs GraphQL e o WordPress. Desenvolvo usando PHP, Vue, Sass, Javascript, usando WordPress.
+          </p>
         </div>
         <div class="resume-date text-md-right">
           <span class="text-primary">Atualmente</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Fundador e desenvolvedor</h3>
+          <div class="subheading mb-3">Bazar Indiano (E-Commerce)</div>
+          <p>
+            Criei o Bazar Indiano em 2016 com a proposta de comercializar produtos indianos no Brasil, alguns com exclusividade e com isso diminar um pouco da cultura indiana. Hoje estamos entre as três empresas principais no segmento. Cuido do planejamento da empresa, da infra de tecnologia, estratégia, marketing e SEO. Hoje o Bazar é uma empresa pequena e familiar porém com processos extremamente eficientes e com índices de satisfação excepcionais.
+          </p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">Maio de 2016 - atualmente</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Front-End / FullStack</h3>
+          <div class="subheading mb-3">Freelancer</div>
+          <p>
+            Desde que saí da Mkt Virtual, passei a atuar como freelancer. Trabalhei para diversos clientes e agências como a RockerHeads, do Grupo ABC, o Labirinto Studio, a agência Dooca, Spark44, a PixelPeople, a Señores.co, e alguns projetos próprios.
+          </p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">Maio de 2015 - Atualmente</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Front-End Developer Lead</h3>
+          <div class="subheading mb-3">Mkt Virtual</div>
+          <p>
+            Trabalhei ma Mkt Virtual por 6 anos como lead da equipe, desenvolvendo projetos importantes, como Avon, Família Schurmann, Charlie Brown Jr, Instituto Neymar JR, Copag, LEGO, Sherwin Williams, Instituto Ayrton Senna, entre outras. Era responsável por convencionar os padrões de desenvolvimento entre a equipe de forma a manter a qualidade e nivelar o conhecimento.
+            Trabalhávamos com sites institucionais em WordPress, criando temas do zero (blank theme) e CakePHP para projetos com regras de negócio mais específicas, além de alguns Apps usando Ionic, que era a melhor solução multiplataforma da época para desenvolvimento de Apps.
+          </p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">entre 2008 e 2014</span>
         </div>
       </div>
 
